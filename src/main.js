a8cddd1c7a99bfd25f4d2d334fdda68df235f73a
Krzysztof Piekutowski <krzysztof.piekutowski@gmail.com>
2018-06-02 02:17:04 +0200

    Routing to previous components

--- src/main.js
+++ src/main.js
@@ -1,16 +1,36 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import vueResource from 'vue-resource'
-import App from './App'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+
+import Users from './components/Users'
+import MyComponent from './components/MyComponent'
 
 Vue.config.productionTip = false
 
-Vue.use(vueResource)
+Vue.use(VueResource)
+Vue.use(VueRouter)
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: MyComponent },
+    { path: '/my-component', component: MyComponent },
+    { path: '/users', component: Users }
+  ],
+  mode: 'history',
+  base: __dirname
 })
+
+const app = new Vue({
+  router,
+  template: `
+    <div id="app">
+      <ul>
+        <li><router-link to="/">MyComponent</router-link></li>
+        <li><router-link to="/users">Users</router-link></li>
+      </ul>
+      <router-view></router-view>
+    </div>
+  `
+}).$mount('#app')
